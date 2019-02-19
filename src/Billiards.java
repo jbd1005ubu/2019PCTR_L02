46aa1a28f0f953b0a7711001415f47ce449db9fe
jbd1005ubu <47038245+jbd1005ubu@users.noreply.github.com>
2019-02-19 17:44:05 +0100

    When the balls stop we create new ones.

--- src/Billiards.java
+++ src/Billiards.java
@@ -66,12 +66,22 @@ public class Billiards extends JFrame {
 	private class StartListener implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
+			boolean needNewBalls = true;
+			for (Ball b : balls) {
+				if(b.getdr() != 0){
+					needNewBalls = false;
+				}
+			}
+			if(needNewBalls){
+				hasBeenStopped = true;
+				initBalls();
+			}
 		    if(hasBeenStopped) {
 		        hasBeenStopped = false;
                 ballThreads = new ArrayList<>();
                 for (Ball b : balls) {
-                    ballThreads.add(new BallThread(b));
-                }
+					ballThreads.add(new BallThread(b));
+				}
                 for (BallThread bT : ballThreads) {
                     bT.start();
                 }
